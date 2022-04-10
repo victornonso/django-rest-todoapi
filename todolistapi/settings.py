18d96180be93d3fc5aa95155676a13aab929b553
VictorNonso <victornonso44@gmail.com>
2022-04-10 01:25:48 +0100

    updated

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -14,8 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import tkinter
-root = tkinter.Tk()
+from _tkinter import *
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
